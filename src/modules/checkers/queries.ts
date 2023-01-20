6b9741aa3870b2a744145f365ed108c89b7dac4a
srdtrk <srdtrk@hotmail.com>
2023-01-20 11:33:35 +0300

    added implementations to queries.ts using the generated QueryClient implementation

--- src/modules/checkers/queries.ts
+++ src/modules/checkers/queries.ts
@@ -37,3 +37,65 @@ export interface CheckersExtension {
         ) => Promise<QueryCanPlayMoveResponse>;
     };
 }
+
+export function setupCheckersExtension(base: QueryClient): CheckersExtension {
+    const rpc = createProtobufRpcClient(base);
+    // Use this service to get easy typed access to query methods
+    // This cannot be used for proof verification
+    const queryService = new QueryClientImpl(rpc);
+
+    return {
+        checkers: {
+            getSystemInfo: async (): Promise<SystemInfo> => {
+                const { SystemInfo } = await queryService.SystemInfo({});
+                assert(SystemInfo);
+                return SystemInfo;
+            },
+            getStoredGame: async (
+                index: string
+            ): Promise<StoredGame | undefined> => {
+                const response: QueryGetStoredGameResponse =
+                    await queryService.StoredGame({
+                        index: index,
+                    });
+                return response.storedGame;
+            },
+            getAllStoredGames: async (
+                key: Uint8Array,
+                offset: Long,
+                limit: Long,
+                countTotal: boolean
+            ): Promise<AllStoredGameResponse> => {
+                const response: QueryAllStoredGameResponse =
+                    await queryService.StoredGameAll({
+                        pagination: {
+                            key: key,
+                            offset: offset,
+                            limit: limit,
+                            countTotal: countTotal,
+                            reverse: false,
+                        },
+                    });
+                return {
+                    storedGames: response.storedGame,
+                    pagination: response.pagination,
+                };
+            },
+            canPlayMove: async (
+                index: string,
+                player: Player,
+                from: Pos,
+                to: Pos
+            ): Promise<QueryCanPlayMoveResponse> => {
+                return queryService.CanPlayMove({
+                    gameIndex: index,
+                    player: player,
+                    fromX: Long.fromNumber(from.x),
+                    fromY: Long.fromNumber(from.y),
+                    toX: Long.fromNumber(to.x),
+                    toY: Long.fromNumber(to.y),
+                });
+            },
+        },
+    };
+}
