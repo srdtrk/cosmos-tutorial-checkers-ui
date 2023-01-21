6cf5fd1768a78866ca49774e9c05efe9f536543e
srdtrk <srdtrk@hotmail.com>
2023-01-21 13:04:43 +0300

    added some createGame and playMove tests

--- test/integration/stored-game-action.ts
+++ test/integration/stored-game-action.ts
@@ -4,7 +4,14 @@ import { getSignerFromMnemonic } from "../../src/util/signer";
 import _ from "../../environment";
 import { CheckersSigningStargateClient } from "../../src/checkers_signingstargateclient";
 import { CheckersExtension } from "../../src/modules/checkers/queries";
-import { GasPrice } from "@cosmjs/stargate";
+import { DeliverTxResponse, GasPrice } from "@cosmjs/stargate";
+import Long from "long";
+import { Log } from "@cosmjs/stargate/build/logs";
+import { StoredGame } from "../../src/types/generated/checkers/stored_game";
+import {
+    getCreatedGameId,
+    getCreateGameEvent,
+} from "../../src/types/checkers/events";
 
 describe("StoredGame Action", function () {
     const {
@@ -43,5 +50,65 @@ describe("StoredGame Action", function () {
             }
         );
         checkers = aliceClient.checkersQueryClient!.checkers;
+    });
+
+    // check that a game can be created with a wager:
+    let gameIndex: string;
+
+    it("can create game with wager", async function () {
+        this.timeout(5_000);
+        const response: DeliverTxResponse = await aliceClient.createGame(
+            alice,
+            alice,
+            bob,
+            "token",
+            Long.fromNumber(1),
+            "auto"
+        );
+        const logs: Log[] = JSON.parse(response.rawLog!);
+        expect(logs).to.be.length(1);
+        gameIndex = getCreatedGameId(getCreateGameEvent(logs[0])!);
+        const game: StoredGame = (await checkers.getStoredGame(gameIndex))!;
+        expect(game).to.include({
+            index: gameIndex,
+            black: alice,
+            red: bob,
+            denom: "token",
+        });
+        expect(game.wager.toNumber()).to.equal(1);
+    });
+
+    // make move and pay wager
+
+    it("can play first moves and pay wager", async function () {
+        this.timeout(10_000);
+        const aliceBalBefore = parseInt(
+            (await aliceClient.getBalance(alice, "token")).amount,
+            10
+        );
+        await aliceClient.playMove(
+            alice,
+            gameIndex,
+            { x: 1, y: 2 },
+            { x: 2, y: 3 },
+            "auto"
+        );
+        expect(
+            parseInt((await aliceClient.getBalance(alice, "token")).amount, 10)
+        ).to.be.equal(aliceBalBefore - 1);
+        const bobBalBefore = parseInt(
+            (await aliceClient.getBalance(bob, "token")).amount,
+            10
+        );
+        await bobClient.playMove(
+            bob,
+            gameIndex,
+            { x: 0, y: 5 },
+            { x: 1, y: 4 },
+            "auto"
+        );
+        expect(
+            parseInt((await aliceClient.getBalance(bob, "token")).amount, 10)
+        ).to.be.equal(bobBalBefore - 1);
     });
 });
