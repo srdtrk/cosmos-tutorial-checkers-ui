82d7fcca29cb7a8a772611a391f1a1218c89aa9e
srdtrk <srdtrk@hotmail.com>
2023-01-21 11:35:22 +0300

    keys added to env for testing purposes

--- environment.d.ts
+++ environment.d.ts
@@ -3,6 +3,10 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             RPC_URL: string;
+            MNEMONIC_TEST_ALICE: string;
+            ADDRESS_TEST_ALICE: string;
+            MNEMONIC_TEST_BOB: string;
+            ADDRESS_TEST_BOB: string;
         }
     }
 }
