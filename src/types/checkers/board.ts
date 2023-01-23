74e85c2833d7b9ed1b4ef029ec453654d6040478
srdtrk <srdtrk@hotmail.com>
2023-01-23 11:26:58 +0300

    added function to convert StoredGame object into IGameInfo for the GUI

--- src/types/checkers/board.ts
+++ src/types/checkers/board.ts
@@ -1,3 +1,5 @@
+import { IGameInfo } from "src/sharedTypes";
+import { StoredGame } from "../generated/checkers/stored_game";
 import { Player } from "./player";
 
 const rowSeparator = "|";
@@ -15,4 +17,29 @@ export function serializedToBoard(serialized: string): number[][] {
         .map((row: string) =>
             row.split("").map((char: string) => (pieceTranslator as any)[char])
         );
+}
+
+export function storedToGameInfo(game: StoredGame): IGameInfo {
+    return {
+        board: serializedToBoard(game.board),
+        created: new Date(Date.now()),
+        isNewGame: game.moveCount.equals(0),
+        last: new Date(Date.parse(game.deadline) - 86400 * 1000),
+        p1: {
+            name: game.black, // Addresses are used instead of names.
+            is_ai: false, // To make it simple.
+            score: 0,
+        },
+        p2: {
+            name: game.red,
+            is_ai: false,
+            score: 0,
+        },
+        turn: (pieceTranslator as any)[game.turn],
+        index: parseInt(game.index),
+    };
+}
+
+export function storedsToGameInfos(games: StoredGame[]): IGameInfo[] {
+    return games.map(storedToGameInfo);
 }
