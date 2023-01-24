27df5beb9c7d981591dfb5d8ee811fa48a2a7d43
srdtrk <srdtrk@hotmail.com>
2023-01-24 14:44:45 +0300

    implemented entering new games to database logic

--- src/server/indexer.ts
+++ src/server/indexer.ts
@@ -1,9 +1,17 @@
 import { writeFile } from "fs/promises";
 import { Server } from "http";
 import express, { Express, Request, Response } from "express";
-import { DbType } from "./types";
+import { DbType, PlayerInfo } from "./types";
 import { config } from "dotenv";
 import { CheckersStargateClient } from "../checkers_stargateclient";
+import { Block, IndexedTx } from "@cosmjs/stargate";
+import { sha256 } from "@cosmjs/crypto";
+import { toHex } from "@cosmjs/encoding";
+import {
+    ABCIMessageLog,
+    StringEvent,
+} from "cosmjs-types/cosmos/base/abci/v1beta1/abci";
+import { Attribute } from "@cosmjs/stargate/build/logs";
 
 config();
 
@@ -64,14 +72,170 @@ export const createIndexer = async () => {
 
     const poll = async () => {
         const currentHeight = await client.getHeight();
-        console.log(
-            new Date(Date.now()).toISOString(),
-            "Current heights:",
-            db.status.block.height,
-            "<=",
-            currentHeight
-        );
+        if (db.status.block.height <= currentHeight - 100)
+            console.log(
+                `Catching up ${db.status.block.height}..${currentHeight}`
+            );
+        while (db.status.block.height < currentHeight) {
+            const processing = db.status.block.height + 1;
+            process.stdout.cursorTo(0);
+            // Get the block
+            const block: Block = await client.getBlock(processing);
+            process.stdout.write(
+                `Handling block: ${processing} with ${block.txs.length} txs`
+            );
+            // Function yet to be declared
+            await handleBlock(block);
+            db.status.block.height = processing;
+        }
+        await saveDb();
         timer = setTimeout(poll, pollIntervalMs);
+    };
+
+    const handleBlock = async (block: Block) => {
+        if (0 < block.txs.length) console.log("");
+        let txIndex = 0;
+        while (txIndex < block.txs.length) {
+            const txHash: string = toHex(
+                sha256(block.txs[txIndex])
+            ).toUpperCase();
+            const indexed: IndexedTx | null = await client.getTx(txHash);
+            if (!indexed)
+                throw new Error(`Could not find indexed tx: ${txHash}`);
+            // Function yet to be declared
+            await handleTx(indexed);
+            txIndex++;
+        }
+        // TODO handle EndBlock
+    };
+
+    const handleTx = async (indexed: IndexedTx) => {
+        const rawLog: any = JSON.parse(indexed.rawLog);
+        const events: StringEvent[] = rawLog.flatMap(
+            (log: ABCIMessageLog) => log.events
+        );
+        // Function yet to be declared
+        await handleEvents(events);
+    };
+
+    const handleEvents = async (events: StringEvent[]): Promise<void> => {
+        try {
+            let eventIndex = 0;
+            while (eventIndex < events.length) {
+                // Function yet to be declared
+                await handleEvent(events[eventIndex]);
+                eventIndex++;
+            }
+        } catch (e) {
+            // Skipping if the handling failed. Most likely the transaction failed.
+        }
+    };
+
+    const handleEvent = async (event: StringEvent): Promise<void> => {
+        if (event.type == "new-game-created") {
+            // Function yet to be declared
+            await handleEventCreate(event);
+        }
+        if (event.type == "game-rejected") {
+            // Function yet to be declared
+            await handleEventReject(event);
+        }
+        if (event.type == "move-played") {
+            // Function yet to be declared
+            await handleEventPlay(event);
+        }
+    };
+
+    const getAttributeValueByKey = (
+        attributes: Attribute[],
+        key: string
+    ): string | undefined => {
+        return attributes.find((attribute: Attribute) => attribute.key === key)
+            ?.value;
+    };
+
+    const handleEventCreate = async (event: StringEvent): Promise<void> => {
+        const newId: string | undefined = getAttributeValueByKey(
+            event.attributes,
+            "game-index"
+        );
+        if (!newId) throw new Error(`Create event missing game-index`);
+        const blackAddress: string | undefined = getAttributeValueByKey(
+            event.attributes,
+            "black"
+        );
+        if (!blackAddress)
+            throw new Error(`Create event missing black address`);
+        const redAddress: string | undefined = getAttributeValueByKey(
+            event.attributes,
+            "red"
+        );
+        if (!redAddress) throw new Error(`Create event missing red address`);
+        console.log(
+            `New game: ${newId}, black: ${blackAddress}, red: ${redAddress}`
+        );
+        const blackInfo: PlayerInfo = db.players[blackAddress] ?? {
+            gameIds: [],
+        };
+        const redInfo: PlayerInfo = db.players[redAddress] ?? {
+            gameIds: [],
+        };
+        if (blackInfo.gameIds.indexOf(newId) < 0) blackInfo.gameIds.push(newId);
+        if (redInfo.gameIds.indexOf(newId) < 0) redInfo.gameIds.push(newId);
+        db.players[blackAddress] = blackInfo;
+        db.players[redAddress] = redInfo;
+        db.games[newId] = {
+            redAddress: redAddress,
+            blackAddress: blackAddress,
+            deleted: false,
+        };
+    };
+
+    const handleEventReject = async (event: StringEvent): Promise<void> => {
+        const rejectedId: string | undefined = getAttributeValueByKey(
+            event.attributes,
+            "game-index"
+        );
+        if (!rejectedId) throw new Error(`Reject event missing game-index`);
+        const blackAddress: string | undefined =
+            db.games[rejectedId]?.blackAddress;
+        const redAddress: string | undefined = db.games[rejectedId]?.redAddress;
+        console.log(
+            `Reject game: ${rejectedId}, black: ${blackAddress}, red: ${redAddress}`
+        );
+        const blackGames: string[] = db.players[blackAddress]?.gameIds ?? [];
+        const redGames: string[] = db.players[redAddress]?.gameIds ?? [];
+        const indexInBlack: number = blackGames.indexOf(rejectedId);
+        if (0 <= indexInBlack) blackGames.splice(indexInBlack, 1);
+        const indexInRed: number = redGames.indexOf(rejectedId);
+        if (0 <= indexInRed) redGames.splice(indexInRed, 1);
+        if (db.games[rejectedId]) db.games[rejectedId].deleted = true;
+    };
+
+    const handleEventPlay = async (event: StringEvent): Promise<void> => {
+        const playedId: string | undefined = getAttributeValueByKey(
+            event.attributes,
+            "game-index"
+        );
+        if (!playedId) throw new Error(`Play event missing game-index`);
+        const winner: string | undefined = getAttributeValueByKey(
+            event.attributes,
+            "winner"
+        );
+        if (!winner) throw new Error("Play event missing winner");
+        if (winner === "*") return;
+        const blackAddress: string | undefined =
+            db.games[playedId]?.blackAddress;
+        const redAddress: string | undefined = db.games[playedId]?.redAddress;
+        console.log(
+            `Win game: ${playedId}, black: ${blackAddress}, red: ${redAddress}, winner: ${winner}`
+        );
+        const blackGames: string[] = db.players[blackAddress]?.gameIds ?? [];
+        const redGames: string[] = db.players[redAddress]?.gameIds ?? [];
+        const indexInBlack: number = blackGames.indexOf(playedId);
+        if (0 <= indexInBlack) blackGames.splice(indexInBlack, 1);
+        const indexInRed: number = redGames.indexOf(playedId);
+        if (0 <= indexInRed) redGames.splice(indexInRed, 1);
     };
 
     process.on("SIGINT", () => {
